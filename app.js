1536fffbaa07c1658b951e9c6f9708363d28a3db
AkwGaniu <lmd4sure@gmail.com>
2021-03-30 10:09:01 +0100

    little fix

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ app.get( '/', ( req, res ) => {
 io.of( '/stream' ).on( 'connection', stream );
 
 const PORT = process.env.PORT || 3000
-server.listen( 3000, () => {
+server.listen( PORT, () => {
     console.log(`Server up on port ${PORT}`)
 })
